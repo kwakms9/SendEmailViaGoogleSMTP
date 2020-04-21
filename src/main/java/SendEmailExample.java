f713be30b15ecd95dd07e05bfaa2a56b4073db91
KMS <good6676g@gmail.com>
2020-04-22 00:14:53 +0900

    updated input mail data from console

--- src/main/java/SendEmailExample.java
+++ src/main/java/SendEmailExample.java
@@ -4,21 +4,46 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * @author yuchlin on 3/21/20
  */
 public class SendEmailExample {
-    public static void main(String[] args){
-
+    public static void main(String[] args){ 
+ 
+        Scanner scan = new Scanner(System.in);
+        String username;
+        String password;
+        String receiver;
+        String title;
+        String content;
+        String yesorno;
+        
+        
+        System.out.println("구글이메일을 입력하시오: ");
+        username = scan.next();
+        System.out.println("비밀번호를 입력하시오: ");
+        password = scan.next();
+        System.out.println("보낼사람의 이메일 주소를 입력하시오: ");
+        receiver = scan.next();
+        
+        while(true) {
+        	if(true) {
+        		System.out.println("제목을 입력하시오: ");
+        		title = scan.nextLine();
+        		System.out.println("내용을 입력하시오: ");
+        		content = scan.nextLine();
+        		System.out.println("전송하시겠습니까?(Y:전송 N:다시입력):");
+        		yesorno = scan.next();
+        		if(yesorno.equalsIgnoreCase("Y")) {
+        			break;
+        		}
+        	}
+        }
+        
         try{
 
-            final String username = "your gmail";
-            final String password = "your gmail credential";
-            String receiver = "receiver email adress";
-            
-            
             Properties prop = new Properties();
             prop.put("mail.smtp.host", "smtp.gmail.com");
             prop.put("mail.smtp.port", "587");
@@ -38,11 +63,11 @@ public class SendEmailExample {
                     Message.RecipientType.BCC,
                     InternetAddress.parse(receiver)
             );
-            message.setSubject("Your email title");
+            message.setSubject(title);
 
 
 
-            message.setContent("Hi", "text/html");
+            message.setContent(content, "text/html");
 
             Transport.send(message);
 
